e6ed05f6d3e26d3639615d7188e08c7469713d9b
Sergio <secwinix@gmail.com>
2024-06-06 01:25:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const crypto = require('crypto');
-const { analyzeUrl, getCountryInfo } = require('.utils/siteAnalysis');
+const { analyzeUrl, getCountryInfo } = require('./utils/siteAnalysis');
 
 // Configuración de la API de Twitter
 const twitterClient = new TwitterApi({
